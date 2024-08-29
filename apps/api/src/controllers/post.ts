497d4f23e3a3c3580efdb9f50f9b7beb8b5a83b7
Jessica Perez <jessica.perez@accenture.com>
2024-08-29 16:44:13 -0600

    sea agrega lo del mongo

--- apps/api/src/controllers/post.ts
+++ apps/api/src/controllers/post.ts
@@ -1,151 +1,95 @@
 import { RequestHandler } from 'express';
-import { Post, Comment } from '../models/posts';
-
-const posts: Post[] = [];
-// const comments: Comment[] = [];
-
-export const getByPost = (id: string) => {
-  return posts.find((p) => p.id === id);
-};
-
-export const getPostbyCategory = (category: string) => {
-  return posts.find((p) => p.category === category);
-};
+import Post from '../models/posts';
+import Comment from '../models/comment';
 
 // Get all posts
-const getAllPosts: RequestHandler = (req, res) => {
+const getAllPosts: RequestHandler = async (req, res) => {
   // Return all the posts with a 200 status code
-  res.status(200).json(posts);
+  try {
+    const posts = await Post.find();
+    return res.status(200).json(posts);
+  } catch (error) {
+    const { message } = error;
+    return res.status(500).json({ message });
+  }
 };
 
 // Get post by category
-const getPostByCategory: RequestHandler = (req, res) => {
-  const { category } = req.params;
-
-  const post = getPostbyCategory(category);
-
-  if (!post) {
-    return res.status(404).json({ message: 'Post not found by category' });
+const getPostByCategory: RequestHandler = async (req, res) => {
+  const { categoryId } = req.params;
+  try {
+    const posts = await Post.find({ category: categoryId });
+    return res.status(200).json(posts);
+  } catch (error) {
+    const { message } = error;
+    return res.status(500).json({ message });
   }
-
-  res.status(200).json(post);
 };
 
 // Get post by id
-const getPostById: RequestHandler = (req, res) => {
+const getPostById: RequestHandler = async (req, res) => {
   const { id } = req.params;
-
-  const post = getByPost(id);
-
-  if (!post) {
-    return res.status(404).json({ message: 'Post not found' });
+  try {
+    const post = await Post.findById(id);
+    return res.status(200).json(post);
+  } catch (error) {
+    const { message } = error;
+    return res.status(500).json({ message });
   }
-
-  res.status(200).json(post);
 };
 
 // Create post
-const createPost: RequestHandler = (req, res) => {
-  const { title, image, description, category } = req.body;
-
-  if (!title) {
-    return res.status(400).json({ message: 'The title is required.' });
+const createPost: RequestHandler = async (req, res) => {
+  const postData = req.body;
+  try {
+    const newPost = await Post.create(postData);
+    return res.status(201).json(newPost);
+  } catch (error) {
+    const { message } = error;
+    return res.status(500).json({ message });
   }
-  if (!image) {
-    return res.status(400).json({ message: 'The image is required.' });
-  }
-  if (!description) {
-    return res.status(400).json({ message: 'The description is required.' });
-  }
-  if (!category) {
-    return res.status(400).json({ message: 'The category is required.' });
-  }
-
-  // Generate a new post
-  const newPosts = {
-    id: Date.now().toString(), // Convert id to string to match the value in get by id endpoint
-    title,
-    image,
-    description,
-    category,
-    comments: []
-  };
-  // Add the new post to our array
-  posts.push(newPosts);
-
-  // Return the created post with a 201 status code
-  res.status(201).json(newPosts);
 };
 
 // Create comments
-const createComments: RequestHandler = (req, res) => {
-  const { id } = req.params
-  const { author, content } = req.body;
-
-  if (!author) {
-    return res.status(400).json({ message: 'The author is required.' });
+const createComments: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  const commentData = req.body;
+  try {
+    const post = await Post.findById(id);
+    const comment = await Comment.create(commentData);
+    post.comments.push(comment._id);
+    await post.save();
+    return res.status(201).json(comment);
+  } catch (error) {
+    const { message } = error;
+    return res.status(500).json({ message });
   }
-  if (!content) {
-    return res.status(400).json({ message: 'The content is required.' });
-  }
-
-  const newComment: Comment = {
-    id: id,
-    author: author,
-    content: content
-  }
-
-  // Add the new comment to our array
-  posts[id].comments.push(newComment)
-
-  // Return the created comment with a 201 status code
-  res.status(201).json(newComment);
 };
 
 // Update posts by id
-const updatePostById: RequestHandler = (req, res) => {
-  const { id } = req.params
-
-  const postbyId = posts.findIndex((p) => p.id === id);
-
-  if (postbyId === -1) {
-    // If we don't find the posts return a 404 status code with a message
-    return res.status(404).json({ message: 'Post not found' });
+const updatePostById: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  const newPostData = req.body;
+  try {
+    const modifiedPost = await Post.findByIdAndUpdate(id, newPostData, { new: true, runValidators: true });
+    return res.status(200).json(modifiedPost);
+  } catch (error) {
+    const { message } = error;
+    return res.status(500).json({ message });
   }
-
-  // Generate a copy of our posts
-  const updatedPost = { ...posts[postbyId] };
-  const { title } = req.body;
-
-  if (title) {
-    updatedPost.title = title;
-  }
-
-  /// Update the posts in our array
-  posts[postbyId] = updatedPost;
-
-  // Return the updated post with a 201 status code
-  res.status(201).json(updatedPost);
 };
 
 // Delete post by id
-const deletePost: RequestHandler = (req, res) => {
-  // Retrieve the id from the route params
+const deletePost: RequestHandler = async (req, res) => {
   const { id } = req.params;
-  // Retrieve the index of the post in the array
-  const postIndex = posts.findIndex((p) => p.id === id);
-
-  // "findIndex" will return -1 if there is no match
-  if (postIndex === -1) {
-    // If we don't find the post return a 404 status code with a message
-    return res.status(404).json({ message: 'Post not found' });
+  try {
+    const deletedPost = await Post.findByIdAndDelete(id, {});
+    await Comment.deleteMany({ _id: { $in: deletedPost.comments } });
+    return res.status(200).json(deletedPost);
+  } catch (error) {
+    const { message } = error;
+    return res.status(500).json({ message });
   }
-
-  // Remove the post from the array
-  posts.splice(postIndex, 1);
-
-  // Return a 204 status code
-  res.status(204).send();
 };
 
 export default {
--- apps/api/src/models/posts.ts
+++ apps/api/src/models/posts.ts
@@ -1,14 +1,44 @@
-export type Comment = {
-  id: string;
-  author: string;
-  content: string;
-};
+import mongoose, { Document, Schema } from 'mongoose';
 
-export type Post = {
-  id: string;
+interface IPost extends Document {
   title: string;
   image: string;
   description: string;
   category: string;
-  comments: Comment[];
-};
+  comments: string[];
+}
+
+export const postSchema = new Schema<IPost>(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+      maxlength: [256, 'title should be no greater than 256 characters']
+    },
+    image: {
+      type: String,
+      required: [true, 'image URL is missing']
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required']
+    },
+    category: {
+      type: String,
+      required: [true, 'Category Id is missing']
+    },
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Post = mongoose.model<IPost>('Post', postSchema);
+
+export default Post;
